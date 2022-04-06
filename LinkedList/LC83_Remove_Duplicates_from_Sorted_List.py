08ab4fbe3f0d0b791e8d9ad60551dc8c5ffd310b
dyh <yuhao.dai@envision-digital.com>
2022-04-06 10:59:56 +0800

    Finish 83

---
+++ LinkedList/LC83_Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        '''
+        cur & cur.next not None
+        need to check cases: [1,1,1,1]
+        '''
+
+        if head is None:
+            return None
+        cur = head
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+        return head
